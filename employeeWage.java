2d71501936729da9516c03784b6e5b0953b523a5
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 13:55:16 +0530

    Added:Added the program to check the Employee attendance.

--- employeeWage.java
+++ employeeWage.java
@@ -1,5 +1,14 @@
+import java.util.Random;
 class employeeWage {
 	public static void main(String args[]){
 		System.out.println("Welcome to Employee Wage Computation Program");
+		Random rand = new Random();
+		int randomNum = rand.nextInt(2);
+		if(randomNum==0) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
