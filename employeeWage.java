621ab859d42950e66916314a14f98defe1b4ef8f
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 14:13:31 +0530

    Added:Added program to compute monthly wage.

--- employeeWage.java
+++ employeeWage.java
@@ -5,20 +5,28 @@ class employeeWage {
 		int fullDayHours = 8;
 		int wagePerHour = 20;
 		int partTimeHours = 4;
+		int dailyWage = 0;
+		int workingDays = 20;
+		int monthlyWage = 0;
 		Random rand = new Random();
-		int randomNum = rand.nextInt(3);
-		switch (randomNum) {
-		case 0:
-			int dailyWage = fullDayHours * wagePerHour;
-			System.out.println("Employee is Present! DailyWage is "+dailyWage);
-			break;
-		case 1:
-			int dailywage = partTimeHours * wagePerHour;
-			System.out.println("Employee is Present for Halfday. Dailywage is "+dailywage);
-			break;
-		case 2:
-			System.out.println("Employee is Absent");
-			break;
+		for(int i=0; i<=workingDays; i++) {
+			int randomNum = rand.nextInt(3);
+			switch (randomNum) {
+			case 0:
+				dailyWage = fullDayHours * wagePerHour;
+				System.out.println("Employee is Present for full day! DailyWage is "+dailyWage);
+				monthlyWage = monthlyWage + dailyWage;
+				break;
+			case 1:
+				dailyWage = partTimeHours * wagePerHour;
+				System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
+				monthlyWage = monthlyWage + dailyWage;
+				break;
+			case 2:
+				System.out.println("Employee is Absent");
+				break;
+			}
+		System.out.println("Monthly Wage of the Employee is "+monthlyWage);
 		}
 	}
 }
