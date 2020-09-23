f62436c6ae5ddbc99c71802fb7febad240a411ab
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 13:58:23 +0530

    Added:Added program to compute dailywage.

--- employeeWage.java
+++ employeeWage.java
@@ -1,11 +1,14 @@
 import java.util.Random;
-class employeeWage {
+class employeeWage { 
 	public static void main(String args[]){
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int fullDayHours = 8;
+		int wagePerHour = 20;
 		Random rand = new Random();
 		int randomNum = rand.nextInt(2);
 		if(randomNum==0) {
-			System.out.println("Employee is Present");
+			int dailyWage = fullDayHours * wagePerHour;
+			System.out.println("Employee is Present! DailyWage is "+dailyWage);
 		}
 		else {
 			System.out.println("Employee is Absent");
