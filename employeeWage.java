62743a615a8a525b09d123c9a87f0a6ffc8d3dee
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 14:18:04 +0530

    Added the limitation of hours and working days.

--- employeeWage.java
+++ employeeWage.java
@@ -8,25 +8,31 @@ class employeeWage {
 		int dailyWage = 0;
 		int workingDays = 20;
 		int monthlyWage = 0;
+		int workingHours = 0;
 		Random rand = new Random();
 		for(int i=0; i<=workingDays; i++) {
-			int randomNum = rand.nextInt(3);
-			switch (randomNum) {
-			case 0:
-				dailyWage = fullDayHours * wagePerHour;
-				System.out.println("Employee is Present for full day! DailyWage is "+dailyWage);
-				monthlyWage = monthlyWage + dailyWage;
-				break;
-			case 1:
-				dailyWage = partTimeHours * wagePerHour;
-				System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
-				monthlyWage = monthlyWage + dailyWage;
-				break;
-			case 2:
-				System.out.println("Employee is Absent");
-				break;
+			if(workingHours<=100) {
+				int randomNum = rand.nextInt(3);
+				switch (randomNum) {
+				case 0:
+					dailyWage = fullDayHours * wagePerHour;
+					System.out.println("Employee is Present! DailyWage is "+dailyWage);
+					monthlyWage = monthlyWage + dailyWage;
+					workingHours = workingHours + fullDayHours;
+					break;
+				case 1:
+					dailyWage = partTimeHours * wagePerHour;
+					System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
+					monthlyWage = monthlyWage + dailyWage;
+					workingHours = workingHours + partTimeHours;
+					break;
+				case 2:
+					System.out.println("Employee is Absent");
+					break;
+				}
+				System.out.println("Monthly Wage of the Employee is "+monthlyWage);
+				System.out.println("Total Working Hours is "+workingHours);
 			}
-		System.out.println("Monthly Wage of the Employee is "+monthlyWage);
 		}
 	}
 }
