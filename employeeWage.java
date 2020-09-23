1cac8e73971f07291e88762b820ef2b8365e41d4
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 14:03:05 +0530

    Added:Added program to compute dailywage for parttime works.

--- employeeWage.java
+++ employeeWage.java
@@ -1,14 +1,19 @@
 import java.util.Random;
-class employeeWage { 
+class employeeWage {
 	public static void main(String args[]){
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int fullDayHours = 8;
 		int wagePerHour = 20;
+		int partTimeHours = 4;
 		Random rand = new Random();
-		int randomNum = rand.nextInt(2);
+		int randomNum = rand.nextInt(3);
 		if(randomNum==0) {
 			int dailyWage = fullDayHours * wagePerHour;
 			System.out.println("Employee is Present! DailyWage is "+dailyWage);
+		}
+		else if(randomNum==1) {
+			int dailyWage = partTimeHours * wagePerHour;
+			System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
 		}
 		else {
 			System.out.println("Employee is Absent");
