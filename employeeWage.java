f65092a6629b091ad6c0cd99bf2396b7a55ba213
Santhosh V <santhosh.v6426@gmail.com>
2020-09-23 14:24:25 +0530

    Refactor:Refactored the program using class method.

--- employeeWage.java
+++ employeeWage.java
@@ -1,27 +1,27 @@
 import java.util.Random;
-class employeeWage {
-	public static void main(String args[]){
-		System.out.println("Welcome to Employee Wage Computation Program");
-		int fullDayHours = 8;
-		int wagePerHour = 20;
-		int partTimeHours = 4;
-		int dailyWage = 0;
-		int workingDays = 20;
-		int monthlyWage = 0;
-		int workingHours = 0;
-		Random rand = new Random();
+public class employeeWage {
+	int fullDayHours = 8;
+	int wagePerHour = 20;
+	int partTimeHours = 4;
+	int dailyWage = 0;
+	int workingDays = 20;
+	public int monthlyWage;
+	public int workingHours;
+	Random rand = new Random();
+	
+	public void monthlyWages() {
 		for(int i=0; i<=workingDays; i++) {
 			if(workingHours<=100) {
 				int randomNum = rand.nextInt(3);
 				switch (randomNum) {
 				case 0:
 					dailyWage = fullDayHours * wagePerHour;
-					System.out.println("Employee is Present! DailyWage is "+dailyWage);
+					System.out.println("Employee is Present Fullday! DailyWage is "+dailyWage);
 					monthlyWage = monthlyWage + dailyWage;
 					workingHours = workingHours + fullDayHours;
 					break;
 				case 1:
-					dailyWage = partTimeHours * wagePerHour;
+					int dailyWage = partTimeHours * wagePerHour;
 					System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
 					monthlyWage = monthlyWage + dailyWage;
 					workingHours = workingHours + partTimeHours;
@@ -35,4 +35,30 @@ class employeeWage {
 			}
 		}
 	}
-}
+		public static void main(String args[]) {
+			System.out.println("Welcome to Employee Wage Computation Program");
+			employeeWage obj=new employeeWage();
+			System. out. println(); 
+			obj.monthlyWage=0;
+			obj.workingHours=0;
+			System.out.println("Month January");
+			obj.monthlyWages();
+			System. out. println(); 
+			obj.monthlyWage=0;
+			obj.workingHours=0;
+			System.out.println("Month February");
+			obj.monthlyWages();
+			System. out. println(); 
+			obj.monthlyWage=0;
+			obj.workingHours=0;
+			System.out.println("Month March");
+			obj.monthlyWages();
+			System. out. println(); 
+			obj.monthlyWage=0;
+			obj.workingHours=0;
+			System.out.println("Month April");
+			obj.monthlyWages();
+			
+		}
+		
+	}
