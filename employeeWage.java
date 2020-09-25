754a9c6bdba9736ced4c887a5539aa388ff35c03
Santhosh V <santhosh.v6426@gmail.com>
2020-09-25 12:57:27 +0530

    Added:Added program to save the details of the multiple companies.

--- employeeWage.java
+++ employeeWage.java
@@ -1,8 +1,8 @@
 import java.util.*;
-import java.util.Random;
 public class employeeWage {
-	public String organization;
 	int fullDayHours = 8;
+	public static int numCompany;
+	private static ArrayList<Integer> org;
 	public int wagePerHour;
 	int partTimeHours = 4;
 	int dailyWage = 0;
@@ -10,8 +10,8 @@ public class employeeWage {
 	public int monthlyWage;
 	public int workingHours;
 	int hours=0;
-	Random rand = new Random();
-	
+	Random rand = new Random();	
+	static ArrayList<Integer> totalWage = new ArrayList<Integer>();
 	public void monthlyWages() {
 		for(int i=0; i<=workingDays; i++) {
 			if(hours<=workingHours) {
@@ -19,41 +19,56 @@ public class employeeWage {
 				switch (randomNum) {
 				case 0:
 					dailyWage = fullDayHours * wagePerHour;
-					System.out.println("Employee is Present Fullday! DailyWage is "+dailyWage);
 					monthlyWage = monthlyWage + dailyWage;
 					hours = hours + fullDayHours;
 					break;
 				case 1:
 					int dailyWage = partTimeHours * wagePerHour;
-					System.out.println("Employee is Present for Halfday. Dailywage is "+dailyWage);
 					monthlyWage = monthlyWage + dailyWage;
 					hours = hours + partTimeHours;
 					break;
 				case 2:
-					System.out.println("Employee is Absent");
 					break;
 				}
-				System.out.println("Monthly Wage of the Employee is "+monthlyWage);
-				System.out.println("Total Working Hours is "+hours);
+				}
+			
 			}
-		}
-	}
+    
+}
 		public static void main(String args[]) {
 			System.out.println("Welcome to Employee Wage Computation Program");
 			employeeWage obj=new employeeWage();
 			Scanner sc= new Scanner(System.in);
-			System.out.print("Enter the namee of the Organization : ");
-			obj.organization=sc.nextLine();
-			System.out.print("Enter max number of working days in a month- ");
-			obj.workingDays=sc.nextInt();
-			obj.monthlyWage=0;
-			System.out.print("Enter max number of Working Hours in a month- ");
-			obj.workingHours=sc.nextInt();
-			System.out.print("Enter Wage per hour - ");
-			obj.wagePerHour=sc.nextInt();
-			System. out. println();
-			System.out.println("Organization : "+obj.organization);
-			obj.monthlyWages();
+			System.out.println("Enter number of commpany details to be entered : ");
+			numCompany=sc.nextInt();
+			ArrayList<Integer> totalWage = new ArrayList<Integer>();
+			ArrayList<String> org = new ArrayList<String>();
+			ArrayList<Integer> hr = new ArrayList<Integer>();
+			for(int j=1;j<=numCompany; j++) {
+				sc.nextLine();
+				System.out.println("Enter the name of the Organization - ");
+				String organization = sc.nextLine();
+				System.out.println();
+				System.out.print("Enter max number of working days in a month - ");
+				obj.workingDays=sc.nextInt();
+				obj.monthlyWage=0;
+				obj.hours=0;
+				System.out.print("Enter max number of Working Hours in a month - ");
+				obj.workingHours=sc.nextInt();
+				System.out.print("Enter Wage per hour - ");
+				obj.wagePerHour=sc.nextInt();
+				System. out. println();
+				obj.monthlyWages();	
+				org.add(organization);	
+				totalWage.add(obj.monthlyWage);
+				hr.add(obj.hours);
+				
+			}
+			for(int k=1; k<=numCompany; k++) {
+			System.out.println("Organization = "+org.get(k-1));
+			System.out.println("Monthlywage = "+totalWage.get(k-1)+" TotalHours = "+hr.get(k-1));
+			}
+			sc.close();
 			
 		}
 		
